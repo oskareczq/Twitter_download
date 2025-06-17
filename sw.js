33c5f662f03755f78e303515f0bdb05e72b19691
Oskar Wiśniewski <oskarwisniewski3@gmail.com>
2025-06-18 01:50:27 +0200

    fix pwa

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
 const CACHE_NAME = 'x-downloader-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/manifest.json'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
@@ -17,6 +19,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
+                // Return cached version or fetch from network
                 return response || fetch(event.request);
             })
     );
